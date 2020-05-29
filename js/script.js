37c5742dc29a6e8fdb6eff182c8656be19a61f1f
Ricardo Sanchez <ricardo.sanchez@vhb.com>
2020-05-29 07:45:25 -0500

    add carousel

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
 $(document).ready(function () {
-  if($(".content-container").prop('scrollHeight') < $(".content-container").outerHeight()){
-    $(".content-container").removeClass("overflow");
-  }
   $(".navbar > a").click(function (e) {
     e.preventDefault();
     goToDiv(this.id);
   });
-  console.log($(".content-container").outerHeight());
-  console.log($(".content-container").prop('scrollHeight'))
+  navbarScroll();
+});
+
+function navbarScroll(){
   $(".content-container").scroll(function () {
     if ($(this).scrollTop() > 0) {
-      $(".header").animate({
-        top: '-165px'
-      })
-      $(".navbar").animate({
-        top: '-165px'
-      })
-      $(".content-container").animate({
-        marginTop: '30px'
-      })
+      $(".header").css(
+        'top', '-165px')
+      $(".navbar").css(
+        'top', '-165px'
+      )
+      $(".content-container").css(
+        'margin-top', '30px'
+      )
     } else if ($(this).scrollTop() === 0) {
-      console.log($(this).scrollTop());
-      $(".header").animate({
-        top: '0px'
-      })
-      $(".navbar").animate({
-        top: '0px'
-      })
-      $(".content-container").animate({
-        marginTop: '130px'
-      })
+      $(".header").css(
+        'top', '0px'
+      )
+      $(".navbar").css(
+        'top', '0px'
+      )
+      $(".content-container").css(
+        'margin-top', '195px'
+      )
     }
   });
-});
+}
 
 function goToDiv(id) {
   let idToScroll = id.replace("Link", "");
   $(".navbar > a").removeClass("active");
   $("#" + id).addClass("active");
-  $("html,body").animate(
+  $(".content-container").animate(
     {
       scrollTop: $("#" + idToScroll).offset().top,
     },
